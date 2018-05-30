7e58aee3db8f2a9e8d9c80470750f123ca47db32
pgris <philippe.gris@clermont.in2p3.fr>
2018-05-30 02:20:57 -0700

    Add infos

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SN_Utils
+
+# This repo contains usefull classes to run the SN simulation pipeline
--- setups/setups.sh
+++ setups/setups.sh
@@ -2,6 +2,5 @@
 
 #setup lsst_sims
 
-#source /global/common/software/lsst/cori-haswell-gcc/stack/setup_w_2018_13-sims_2_7_0.sh
-source /cvmfs/sw.lsst.eu/linux-x86_64/lsst_sims/sims_2_8_0//loadLSST.bash
+source /global/common/software/lsst/cori-haswell-gcc/stack/w.2018.19_sim2.8.0/loadLSST.bash
 setup lsst_sims
