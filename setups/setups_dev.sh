6c7d16c255b6a70f91565c3f1ddfed14618a6e32
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2018-10-09 15:46:56 +0200

    Add notebook for control plots

--- setups/setups_dev.sh
+++ setups/setups_dev.sh
@@ -5,3 +5,13 @@
 #source /global/common/software/lsst/cori-haswell-gcc/stack/setup_w_2018_13-sims_2_7_0.sh
 source /cvmfs/sw.lsst.eu/linux-x86_64/lsst_sims/sims_2_8_0//loadLSST.bash
 setup lsst_sims
+
+
+
+export PYTHONPATH=${PWD}/SN_Catalog_Simulations/Sim_SNCosmo:$PYTHONPATH
+export PYTHONPATH=${PWD}/SN_Catalog_Simulations/Sim_SNSim:$PYTHONPATH
+export PYTHONPATH=${PWD}/SN_Catalog_Simulations/Sim_SNAna:$PYTHONPATH
+
+export PYTHONPATH=${PWD}/SN_Utils/Utils:$PYTHONPATH
+thedir=${PWD}/lib/python3.6/site-packages
+export PYTHONPATH=${thedir}:$PYTHONPATH
