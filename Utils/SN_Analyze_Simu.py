750f7ff937607f8c723556bf4083138894bab73b
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2018-10-16 12:54:07 +0200

    few corrections

--- Utils/SN_Analyze_Simu.py
+++ Utils/SN_Analyze_Simu.py
@@ -6,13 +6,11 @@ import pylab as plt
 from SN_Rate import SN_Rate
 
 
-def Plot_Parameters(fieldname, fieldid, tab, season):
+def Plot_Parameters(tab, season):
     """ Plot simulation
     parameters ('X1', 'Color', 'DayMax', 'z')
     Input
     ---------
-    fieldname: (DD or WFD)
-    fieldid: (as given by OpSim)
     tab: recarray of parameters
     season: season
 
@@ -26,8 +24,6 @@ def Plot_Parameters(fieldname, fieldid, tab, season):
     thesize = 15
     toplot = ['X1', 'Color', 'DayMax', 'z']
     fig, ax = plt.subplots(ncols=2, nrows=2, figsize=(10, 9))
-    fig.suptitle(fieldname+'  - Fieldid '+str(fieldid) +
-                 ' - season '+str(season+1), fontsize=thesize)
 
     for i, var in enumerate(toplot):
         ix = int(i/2)
@@ -43,12 +39,12 @@ def Plot_Parameters(fieldname, fieldid, tab, season):
             n, bins, patches = axis.hist(sel[var])
             bin_center = (bins[:-1] + bins[1:]) / 2
             dz = bins[1]-bins[0]
-            sn_rate = SN_Rate(rate='Perrett', H0=72, Om0=0.3, survey_area=9.6)
+            sn_rate = SN_Rate(rate='Perrett', H0=72, Om0=0.3)
             zmin = np.min(sel['z'])
             zmax = np.max(sel['z'])
             duration = np.max(sel['DayMax'])-np.min(sel['DayMax'])
             zz, rate, err_rate, nsn, err_nsn = sn_rate(
-                zmin=zmin-dz/2., zmax=zmax, dz=dz, duration=duration)
+                zmin=zmin-dz/2., zmax=zmax, dz=dz, duration=duration,survey_area=9.6)
             axis.plot(zz, np.cumsum(nsn))
             axis.plot(bin_center, np.cumsum(n))
     plt.show()
@@ -57,30 +53,19 @@ def Plot_Parameters(fieldname, fieldid, tab, season):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Simu file analysis')
-    parser.add_argument('--fieldname',
-                        default='DD',
-                        help='fieldname - DD or WFD')
-    parser.add_argument('--fieldid',
-                        default=100,
-                        help='fieldid of the field to be processed')
-    parser.add_argument('--dir_input',
-                        default='/home/philippe/LSST/SN_pipelines/SN_Catalog_Simulations/Output_Simu/',
-                        help='input files directory')
-    parser.add_argument('--season',
-                        default=0,
-                        help='Season to process (start at 0/-1 = all)')
+    parser.add_argument('--filename',
+                        default='',
+                        help='filename to analyze')
 
     args = parser.parse_args()
 
     # load the hdf5 file
-    fieldname = args.fieldname
-    fieldid = args.fieldid
-    file_name = args.dir_input+'/Simu_'+fieldname + \
-        '_'+str(fieldid)+'_'+str(args.season)+'.hdf5'
-    f = h5py.File(file_name, 'r')
+    filename = args.filename
+    
+    f = h5py.File(filename, 'r')
     print(f.keys())
     for i, key in enumerate(f.keys()):
-        summary = Table.read(file_name, path=key)
+        summary = Table.read(filename, path=key)
         print(len(summary), summary.dtype)
         for season in np.unique(summary['season']):
-            Plot_Parameters(fieldname, fieldid, summary, season)
+            Plot_Parameters(summary, season)
